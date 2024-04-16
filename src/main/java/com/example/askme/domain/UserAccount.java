f99f3c2c44b174a45df76ac14e86382a94186422
JongMinCh0i <wonderland314592@gmail.com>
2024-04-16 12:02:35 +0900

    feat[#3]: 유저 도메인 구현
    - UserAccount 엔티티 클래스에 새로운 필드(userId, userPassword, nickname, email, questionCount, imageUrl)를 추가하여 사용자 프로필 관리 기능을 구현
    - 객체 생성 시 초기값을 설정할 수 있도록 비공개 생성자와 정적 팩토리 메소드 `createUser`를 구현
    - questionCount 필드에 기본값 0을 설정하기 위해 @ColumnDefault 어노테이션을 사용
    - 차후 로그인 기능 구현시 `BCryptPasswordEncoder` 를 활용한 비밀번호(userPassword) 암호화 구현 예정

---
+++ src/main/java/com/example/askme/domain/UserAccount.java
@@ -0,0 +1,53 @@
+package com.example.askme.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserAccount extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false, length = 50)
+    private String userId;
+
+    @Setter
+    @Column(nullable = false)
+    private String userPassword;
+
+    @Setter
+    @Column(nullable = false, length = 100)
+    private String nickname;
+
+    @Setter
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column
+    @ColumnDefault("0")
+    private long questionCount = 0;
+
+    @Setter
+    private String imageUrl;
+
+    private UserAccount(String userId, String nickname, String email, int questionCount, String imageUrl) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.email = email;
+        this.questionCount = questionCount;
+        this.imageUrl = imageUrl;
+    }
+
+    public static UserAccount createUser(String userId, String nickname, String email, int questionCount, String imageUrl) {
+        return new UserAccount(userId, nickname, email, questionCount, imageUrl);
+    }
+}

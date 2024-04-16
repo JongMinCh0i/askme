81a1e3ee0bbf38c121f2d7548e8af964e55e3d97
JongMinCh0i <wonderland314592@gmail.com>
2024-04-16 12:14:39 +0900

    feat[#3]: 게시글 도메인 및 게시글, 댓글의 상태를 표현 하는 enum 클래스 작성
    - Article 엔티티에 새로운 필드 추가(userId, title, content, state, status, imageUrl, viewCount, likeCount) 및 관련 접근자 메서드 구현
    - UserAccount와의 관계 설정을 위한 ManyToOne 매핑 추가
    - 조회수, 좋아요 수에 기본값 0 설정
    - ArticleComment와의 일대다 관계 정의 및 CascadeType.ALL 설정으로 관계된 댓글 자동 관리 활성화
    - SolveState와 ContentStatus enum 클래스 추가하여 문제 해결 상태(해결, 미해결)와 콘텐츠 상태(공개, 수정, 삭제)를 관리
    - 정적 팩토리 메서드 createArticle을 통한 Article 인스턴스 생성 용이화

---
+++ src/main/java/com/example/askme/domain/constant/SolveState.java
@@ -0,0 +1,10 @@
+package com.example.askme.domain.constant;
+
+public enum SolveState {
+
+    SOLVED, NOT_SOLVED_YET; // 게시글 해결, 미해결
+
+    public String getStatus() {
+        return this.name();
+    }
+}

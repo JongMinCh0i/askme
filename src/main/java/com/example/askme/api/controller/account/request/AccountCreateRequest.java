250e6730300c5bc47332a2c1cb8eb4dafb670391
JongMinCh0i <wonderland314592@gmail.com>
2024-05-10 15:18:16 +0900

    feature/Request 객체 Validation Message 작성-#8

--- src/main/java/com/example/askme/api/controller/account/request/AccountCreateRequest.java
+++ src/main/java/com/example/askme/api/controller/account/request/AccountCreateRequest.java
@@ -16,15 +16,15 @@ import lombok.NoArgsConstructor;
 public class AccountCreateRequest {
 
     @NotEmpty(message = "닉네임은 필수입니다.")
-    @Size(max = 50)
+    @Size(max = 50 , message = "닉네임은 50자 이하로 입력해주세요.")
     private String nickname;
 
     @NotEmpty(message = "로그인 Id는 필수입니다.")
-    @Size(max = 50)
+    @Size(max = 50, message = "로그인 Id는 50자 이하로 입력해주세요.")
     private String userId;
 
+    @Size(max = 50, message = "이메일은 50자 이하로 입력해주세요.")
     @Email(message = "이메일 형식이 아닙니다.")
-    @Size(max = 50)
     @NotEmpty(message = "이메일은 필수입니다.")
     private String email;
 

2de1c1a69abb494099429bb264f6a499c1b29973
JongMinCh0i <wonderland314592@gmail.com>
2024-05-13 19:37:39 +0900

    feature/회원 조회 API 작성-#10

--- src/main/java/com/example/askme/api/controller/account/AccountController.java
+++ src/main/java/com/example/askme/api/controller/account/AccountController.java
@@ -1,21 +1,23 @@
 package com.example.askme.api.controller.account;
 
 import com.example.askme.api.controller.account.request.AccountCreateRequest;
+import com.example.askme.api.service.account.AccountInfoService;
 import com.example.askme.api.service.account.AccountService;
 import com.example.askme.api.service.account.response.AccountServiceResponse;
 import com.example.askme.common.ResultResponse;
+import com.example.askme.common.jwt.TokenManager;
+import io.jsonwebtoken.Claims;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/account")
 @RequiredArgsConstructor
 public class AccountController {
 
+    private final TokenManager tokenManager;
+    private final AccountInfoService accountInfoService;
     private final AccountService accountService;
 
     @PostMapping("/signup")
@@ -23,4 +25,13 @@ public class AccountController {
         return ResultResponse.success(accountService.signUp(createRequestAccount.toServiceRequest()));
     }
 
+    @GetMapping("/info")
+    public ResultResponse<AccountServiceResponse> getAccountInfo(
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        String accessToken = authorizationHeader.split(" ")[1];
+        Claims tokenClaims = tokenManager.getTokenClaims(accessToken);
+        Long memberId = Long.valueOf((Integer) tokenClaims.get("memberId"));
+        return ResultResponse.success(accountInfoService.getAccountInfo(memberId));
+    }
 }

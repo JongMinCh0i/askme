152c7e3e0a1edf4532766249529bfd7816e647cc
JongMinCh0i <wonderland314592@gmail.com>
2024-05-13 19:37:25 +0900

    feature/인증 인터셉터 추가-#10

---
+++ src/main/java/com/example/askme/common/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.askme.common.config;
+
+import com.example.askme.common.interceptor.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns(
+                        "/api/oauth/login",
+                        "/api/access-token/issue",
+                        "/api/logout");
+    }
+}

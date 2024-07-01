cda8517aa951492158d2b306536600a8b4cedb45
JongMinCh0i <wonderland314592@gmail.com>
2024-07-01 17:25:22 +0900

    feature/cacheManger 를 활용한 기본 캐시 만료 시간을 1시간 으로 설정-#23

--- src/main/java/com/example/askme/common/config/RedisConfig.java
+++ src/main/java/com/example/askme/common/config/RedisConfig.java
@@ -1,14 +1,19 @@
 package com.example.askme.common.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+import java.time.Duration;
+
+@EnableCaching
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
@@ -29,5 +34,15 @@ public class RedisConfig {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
     }
 }
